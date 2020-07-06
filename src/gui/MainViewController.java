81c6b3f3219cc1a14c54dc543cbb6234f18c9c12
guigomes94 <guigomes94@outlook.com>
2020-07-06 13:54:40 -0300

    AboutView

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,18 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.utils.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 	
@@ -30,7 +37,26 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		
+		loadView("/gui/About.fxml");
+	}
+	
+	private synchronized void loadView(String uri) {
+		var loader = new FXMLLoader(getClass().getResource(uri));
+		try {
+			VBox newVBox = loader.load();
+			
+			var mainScene = Main.getMainScene();
+			var mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			var mainMenu = mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
